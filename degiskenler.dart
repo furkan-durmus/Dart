17b6c5889f7246588c8e544fca8d7f9b4ed533d9
Furkan Durmus <16furkandurmus@gmail.com>
2020-10-01 13:49:44 +0300

    DART

--- degiskenler.dart
+++ degiskenler.dart
@@ -1,7 +1,9 @@
 void main() {
   print("DART");
   String example = "this is string variable";
-  int number = 5 + 5;
+  int number = 5 + 15;
+  bool login = true;
   print(example);
   print(number);
+  print(example + " " + number.toString());
 }
---
+++ if-else.dart
@@ -0,0 +1,9 @@
+void main() {
+  print("DART");
+  String example = "this is strddddddddding variable";
+  int number = 5 + 5;
+  bool login = true;
+  print(example);
+  print(number);
+  print(example + " " + number.toString());
+}
