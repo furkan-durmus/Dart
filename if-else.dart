011abbb8c93008fea27c705cfdd815fab876a3f7
Furkan Durmus <16furkandurmus@gmail.com>
2020-10-04 13:16:35 +0300

    DART

--- if-else.dart
+++ if-else.dart
@@ -1,4 +1,53 @@
 void main() {
-  print("DART");
-  print("DART");
+  bool sistemegiris = false;
+  var sistemegir = false;
+  print(
+      "değişkene false yada true atayınca 'var' olarak belirlesek bile onu bool görür");
+
+  if (sistemegir == true) {
+    print("Girildi");
+  } else {
+    print("true değil");
+  }
+  if (sistemegir == false) {
+    print("Girilmedi!");
+  }
+
+  int puan = 15;
+  if (puan >= 50) {
+    print("Geçti");
+  } else if (puan >= 40) {
+    print("Bütünleme");
+  } else {
+    print("Kaldı");
+  }
+
+  String not = "A";
+  switch (not) {
+    case "A":
+      {
+        print("Süper");
+      }
+      break;
+    case "B":
+      {
+        print("İyi");
+      }
+      break;
+    case "C":
+      {
+        print("İdare eder");
+      }
+      break;
+    case "D":
+      {
+        print("Kötü");
+      }
+      break;
+    default:
+      {
+        print("Bilinmiyor");
+      }
+      break;
+  }
 }
