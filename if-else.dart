b55e3882bd5b5cde38ccdf6e8b2bbdd952719afa
Furkan Durmus <16furkandurmus@gmail.com>
2020-10-04 12:47:50 +0300

    DART

--- if-else.dart
+++ if-else.dart
@@ -1,9 +1,3 @@
 void main() {
   print("DART");
-  String example = "this is strddddddddding variable";
-  int number = 5 + 5;
-  bool login = true;
-  print(example);
-  print(number);
-  print(example + " " + number.toString());
 }
