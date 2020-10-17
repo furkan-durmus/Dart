1a3f8d30ff2901d00f9c0e4ac163e687f4b61c25
Furkan Durmus <16furkandurmus@gmail.com>
2020-10-17 12:51:03 +0300

    DART

--- functions.dart
+++ functions.dart
@@ -1,1 +1,34 @@
-void main() {}
+void main() {
+  sayHi();
+  sayHiTo("Furkan");
+  var result = calculate(5, 10.2);
+  print(result);
+  test(1);
+  test2(num2: 3, num3: 55, num1: 12);
+}
+
+void sayHi() {
+  print("Hi!");
+}
+
+void sayHiTo(String user) {
+  print("hi " + user);
+}
+
+double calculate(double first, double second) {
+  return first * second;
+}
+
+//////////////
+void test(int num1, [int num2, int num3]) {
+  print(num1);
+  print(num2);
+  print(num3);
+}
+
+/////////////
+void test2({int num1, int num2, int num3}) {
+  print(num1);
+  print(num2);
+  print(num3);
+}
